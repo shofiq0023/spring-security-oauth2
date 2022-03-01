fec122323026227f236dcbb3e94b9e3205f1cf3a
shofiq0023 <shofiqulislam0023@gmail.com>
2022-03-01 13:09:11 +0600

    First commit

---
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	@Override
+    protected void configure(HttpSecurity http) throws Exception {
+    	// @formatter:off
+        http
+            .authorizeRequests(a -> a
+                .antMatchers("/", "/error", "/webjars/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .logout(l -> l
+                    .logoutSuccessUrl("/").permitAll()
+             )
+            .exceptionHandling(e -> e
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+            )
+            .csrf(c -> c
+                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                )
+            .oauth2Login();
+            
+        // @formatter:on
+    }
+}
